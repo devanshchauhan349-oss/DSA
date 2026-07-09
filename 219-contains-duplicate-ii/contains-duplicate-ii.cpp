c6066fb930834f679ebe9a50943b2c3e830e10f2
devanshchauhan349-oss <devanshchauhan349@gmail.com>
2026-07-09 12:21:13 +0530

    Time: 72 ms (64.80%) | Memory: 98.7 MB (53.14%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (mp.find(nums[i]) != mp.end()) {
+                if (i - mp[nums[i]] <= k) {
+                    return true;
+                }
+            }
+            mp[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
